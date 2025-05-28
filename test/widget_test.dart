6c7fe66869381124928e2db252386de7fc22f9e2
Phuongthao888 <thaonhilam562@gmail.com>
2025-05-28 17:55:49 +0700

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,8 +10,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:weather_app/my_app.dart';
 
-import 'package:weather_app/page/home/home_page.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
